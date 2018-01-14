e2ff9ddbd08f48c749edb958744d812bbb49ad42
Troy Scott <consult@troyscott.ca>
2018-01-14 10:30:18 -0800

    add the mapGetters make the code less verbose

--- src/components/ShowGreetingsComponent.vue
+++ src/components/ShowGreetingsComponent.vue
@@ -1,12 +1,9 @@
 <template>
-<h1>{{ msg }}</h1>
+<h1>{{ getMessage }}</h1>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
-    computed: {
-        msg() {
-            return this.$store.getters.getMessage
-        }
-    }
+    computed: mapGetters(['getMessage'])
 }
 </script>
